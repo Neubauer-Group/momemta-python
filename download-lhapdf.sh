e8f2c5585b976d928e87ee18fbafa6aa38f71ccc
Matthew Feickert <matthew.feickert@cern.ch>
2020-07-26 23:10:32 -0500

    Remove download-lhapdf Bash script

--- download-lhapdf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-function download_LHAPDF() {
-  # 1: LHAPDF set name
-  local pdfset_name="${1}"
-  lhapdf --source http://lhapdfsets.web.cern.ch/lhapdfsets/current/ get "${pdfset_name}"
-}
-
-download_LHAPDF "$@" || exit 1
