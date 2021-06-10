9b9582963f7bce4f22a2dfc67d4635550b93adaf
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-10 08:41:49 -0500

    Set standard from ROOT always

--- tests/tt_fullyleptonic_tutorial.sh
+++ tests/tt_fullyleptonic_tutorial.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+# bash "scrict mode"
+set -e
+set -u
+set -o pipefail
+
 # Download required LHAPDF set
 lhapdf get CT10nlo
+
+# Get CMAKE_CXX_STANDARD ROOT was built with
+ROOT_CONFIG_CXX=$(root-config --cflags | awk '{print $2}')
+ROOT_CXX_STANDARD="${ROOT_CONFIG_CXX: -2}"
+unset ROOT_CXX_STANDARD
 
 # Clone and build tutorials
 git clone --depth 1 https://github.com/MoMEMta/Tutorials.git \
   --branch v1.0.0 \
   --single-branch
 cmake \
-  -DCMAKE_CXX_STANDARD=17 \
+  -DCMAKE_CXX_STANDARD="${ROOT_CXX_STANDARD}" \
   -S Tutorials \
   -B Tutorials/build
 cmake Tutorials/build -L
@@ -16,7 +26,7 @@ cmake --build Tutorials/build -- -j$(($(nproc) - 1))
 
 # Build Matrix Elements
 cmake \
-  -DCMAKE_CXX_STANDARD=17 \
+  -DCMAKE_CXX_STANDARD="${ROOT_CXX_STANDARD}" \
   -S Tutorials/TTbar_FullyLeptonic/MatrixElement \
   -B Tutorials/TTbar_FullyLeptonic/MatrixElement/build
 cmake Tutorials/TTbar_FullyLeptonic/MatrixElement/build -L
@@ -25,3 +35,5 @@ cmake --build Tutorials/TTbar_FullyLeptonic/MatrixElement/build -- -j$(($(nproc)
 # Run ttbar fully leptonic example
 cd Tutorials/build
 TTbar_FullyLeptonic/TTbar_FullyLeptonic.exe
+
+unset CMAKE_CXX_STANDARD
