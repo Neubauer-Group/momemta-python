71503ef575ede737fbd24289fac1db6d8cd8f7d7
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-02 17:08:30 -0500

    fix: Add MoMEMta-MaGMEE plugin to PYTHONPATH (#14)
    * Add MoMEMta-MaGMEE MadGraph5 plugin to PYTHONPATH
       - Build from `master` branch to pickup https://github.com/MoMEMta/MoMEMta-MaGMEE/pull/10
    * Determine Boost_PYTHON_VERSION_TAG number (in this PR `38`) dynamically to properly build Python bindings
    * Add dependencies Python import pytest tests

---
+++ tests/test_import.py
@@ -0,0 +1,34 @@
+def test_import_madgraph():
+    import madgraph
+
+    assert madgraph
+
+
+def test_import_lhapdf():
+    import lhapdf
+
+    assert lhapdf
+
+
+def test_import_pythia():
+    import pythia8
+
+    assert pythia8
+
+
+def test_import_fastjet():
+    import fastjet
+
+    assert fastjet
+
+
+def test_import_momemta():
+    import momemta
+
+    assert momemta
+
+
+def test_import_ROOT():
+    import ROOT
+
+    assert ROOT
